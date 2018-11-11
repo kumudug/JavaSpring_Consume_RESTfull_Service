1a24d4cae5ea699a12eec356422f700ababd87d8
Kumudu Gunasekara <kumudug1@gmail.com>
2018-11-10 16:12:40 -0800

    Initial project generation using spring initializer

---
+++ src/main/java/com/example/consumingRest/ConsumingRestApplication.java
@@ -0,0 +1,12 @@
+package com.example.consumingRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingRestApplication.class, args);
+	}
+}
